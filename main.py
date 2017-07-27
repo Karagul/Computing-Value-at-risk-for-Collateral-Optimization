64f897acc9210e812a0a4bae9326bc216308d7e4
ndurgai <neerajdurgai718@gmail.com>
2017-07-27 19:50:52 +0100

    Read data function added specifically for historical VaR calculations

--- main.py
+++ main.py
@@ -7,23 +7,23 @@ import numpy as np
 x = readData.alternativeHistReturns(["CHRIS/ICE_B17.1", "CHRIS/ICE_B1.1"], 200, 5)
 #x = readData.alternativeHistReturns("CHRIS/ICE_B17.1", 200, 2)
 weights = [0.7, 0.3]
-
-print("Cor matrix is: ")
+mu = np.mean(x)
+print(mu)
 cov = parametricVar.varCovarMatrix(x)
 print(cov)
+test = monteCarloVaR.pythonNormRand(mu, cov, 1000)
 
-chel = monteCarloVaR.choleskyMatrix(cov)
-print("Chel matrix is: ")
-print(chel)
-chelt = np.transpose(chel)
-print("Chelt matrix is: ")
-print(chelt)
+list1 = []
+list2 = []
+for pair in test:
+    list1.append(pair[0])
+    list2.append(pair[1])
+print("For list 1: ")
+print(np.var(list1))
+print(np.mean(list1))
 
-result = np.dot(chel,chelt)
-print("result matrix is: ")
-print(result)
+print("For list 2: ")
+print(np.var(list2))
+print(np.mean(list2))
 
-print("Cov matrix is: ")
-cov = parametricVar.varCovarMatrix(x)
-print(cov)
-
+print(np.cov(list1, list2))
--- monteCarloVaR.py
+++ monteCarloVaR.py
@@ -5,8 +5,16 @@ import numpy as np
 import scipy.linalg   # SciPy Linear Algebra Library
 
 # Calculates the lower triangular Cholesky matrix (L) of the variance-covariance matrix (E) such that LL^T = E
+# NOTE: Currently not required for multivariate normal distributions as numpy calculates this automatically in
+# numpy.random.multivariate_normal
 def choleskyMatrix(covarmatrix):
     matrix = scipy.linalg.cholesky(covarmatrix,lower=True)
     return matrix
+
+# Python's built in random number generator which builds normally distributed random sequences using the Mersenne
+# Twister algorithm. Returns a numpy array of (n x repetitions), where n is the number of assets
+def pythonNormRand(muvector, cov, repetitions):
+    randarray = np.random.multivariate_normal(muvector, cov, repetitions)
+    return randarray
 
 
