355bfab70d00629f6b8e319a953df67fe86e31c8
ndurgai <neerajdurgai718@gmail.com>
2018-02-27 16:25:57 +0000

    Docstringed the parametricVaR.py, plotData.py, and readData.py modules

--- plotData.py
+++ plotData.py
@@ -2,13 +2,13 @@
 
 import matplotlib.pyplot as plt
 
-# Plot all of the columns of the time series
 def plotSeries(mydata):
+    """Plot all of the columns of the time series """
     plt.plot(mydata)
     plt.show()
 
-# Plot the Open price of the time series
 def plotOpen(mydata):
+    """Plot the Open price of the time series """
     plt.plot(mydata['Open'])
     plt.show()
 
